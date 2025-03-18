e1fb56d80483ba2433f9d9bc5628672b7ae23578
newtonkiragu <karanunewton4@gmail.com>
2025-03-18 13:08:43 +0300

    setup reusable mood enum --WIP

---
+++ src/lib/enum.ts
@@ -0,0 +1,11 @@
+export enum Mood {
+  TERRIBLE = "TERRIBLE",
+  BAD = "BAD",
+  SAD = "SAD",
+  MEH = "MEH",
+  NEUTRAL = "NEUTRAL",
+  OKAY = "OKAY",
+  GOOD = "GOOD",
+  GREAT = "GREAT",
+  HAPPY = "HAPPY"
+}
