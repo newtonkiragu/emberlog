2cabb4cd88d1a8c30531b158bfdd60ebecf7870c
newtonkiragu <karanunewton4@gmail.com>
2025-03-20 17:07:29 +0300

    registered write entry page to perform crud on entries --WIP

--- src/app/write-entry/[id]/page.tsx
+++ src/app/write-entry/[id]/page.tsx
@@ -2,13 +2,13 @@
 
 import {ClientSessionWrapper} from "@/components/CleintSessionWrapper";
 import Navbar from "@/components/navbar/Navbar";
-import Entry from "@/components/entry/WriteEntry";
+import EditDeleteEntry from "@/components/entry/EditDeleteEntry";
 
-export default function WriteEntryPage() {
+export default function ReadEntryPage() {
     return (
         <ClientSessionWrapper>
             <Navbar/>
-            <Entry/>
+            <EditDeleteEntry/>
         </ClientSessionWrapper>
     );
 }
