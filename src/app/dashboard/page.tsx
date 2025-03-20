efb44a52552b106847cfebd2e0db2be67f6d89f9
newtonkiragu <karanunewton4@gmail.com>
2025-03-20 17:11:25 +0300

    removed mui

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,19 +3,14 @@
 import {ClientSessionWrapper} from "@/components/CleintSessionWrapper";
 import Navbar from "@/components/navbar/Navbar";
 import Dashboard from "@/components/dashboard/Dashboard";
-import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFnsV3";
-import {LocalizationProvider} from "@mui/x-date-pickers";
 import {Footer} from "@/components/footer/Footer";
 
 export default function DashboardPage() {
     return (
         <ClientSessionWrapper>
             <Navbar/>
-            <LocalizationProvider dateAdapter={AdapterDateFns as any}>
-                <Dashboard/>
-            </LocalizationProvider>
+            <Dashboard/>
             <Footer/>
-
         </ClientSessionWrapper>
     );
 }
