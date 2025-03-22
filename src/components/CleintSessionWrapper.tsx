a98abf9c81684b03607b8256b48fefac8b169e7d
newtonkiragu <karanunewton4@gmail.com>
2025-03-22 17:18:51 +0300

    just seen typo, but then made client session wrapper use client

--- src/components/CleintSessionWrapper.tsx
+++ src/components/CleintSessionWrapper.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import {SessionProvider} from "next-auth/react";
 import {ReactNode} from "react";
 
-export function ClientSessionWrapper({ children }: { children: ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+export function ClientSessionWrapper({children}: { children: ReactNode }) {
+    return <SessionProvider>{children}</SessionProvider>;
 }
