78e915f57cb157d861676110b670955af0c819b8
newtonkiragu <karanunewton4@gmail.com>
2025-03-22 17:14:26 +0300

    setup summary endpoint

---
+++ src/app/summary/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import Navbar from "@/components/navbar/Navbar";
+import SummaryView from "@/components/summary/Summary";
+import Breadcrumb from "@/components/breadcrumb/Breadcrumb";
+
+export default function RegisterPage() {
+    return (
+        <>
+            <Navbar/>
+            <Breadcrumb/>
+            <SummaryView/>
+        </>
+    );
+}
